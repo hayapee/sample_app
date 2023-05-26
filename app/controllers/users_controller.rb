d026e31b4509eaec66ee27e3ec41d3bf77a81019
hayapee <hayakawa.flute@gmail.com>
2023-05-26 12:04:37 +0000

    fix 10.1.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def show
     @user = User.find(params[:id])
   end
@@ -13,10 +12,23 @@ class UsersController < ApplicationController
     if @user.save
       reset_session
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = 'Welcome to the Sample App!'
       redirect_to @user
     else
       render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # 更新に成功した場合を扱う
+    else
+      render 'edit', status: :unprocessable_entity
     end
   end
 
